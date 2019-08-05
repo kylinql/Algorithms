f3ba768a5b5e84b0a645772b700c56396506bfac
kylin5207 <38106679+kylin5207@users.noreply.github.com>
2019-08-05 18:09:23 +0800

    最长公共子串

--- 09_动态规划/subsentences.py
+++ 09_动态规划/subsentences.py
@@ -13,7 +13,7 @@ def subsentence(str1, str2):
     array = np.zeros((len1,len2))
     for i in range(len1):
         for j in range(len2):
-            if str1[i] == str2[i]:
+            if str1[i] == str2[j]:
                 if i == 0 or j == 0:
                     array[i][j] = 1
                 else:
