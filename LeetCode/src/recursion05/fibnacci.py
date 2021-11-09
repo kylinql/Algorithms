1a9b56e67c9f8dcbf713a98b8e6d8554a3b70047
Kylin <38106679+kylin5207@users.noreply.github.com>
2021-11-09 11:31:18 +0800

    update: reload fibnacci solution of No.509

--- LeetCode/src/recursion05/fibnacci.py
+++ LeetCode/src/recursion05/fibnacci.py
@@ -1,56 +1,24 @@
 # -*- coding:utf-8 -*-
 """
-fibnacci 用迭代器实现斐波那契数列
-迭代器最核心的功能就是可以通过__next__方法的调用来返回下一个值。
-而这个值不是从已有的数据中读取的，而是通过程序按照一定的规则生成的。
-这也就意味着我们可以不再依赖一个现存的数据集合来存放数据，而是边用边生成，这样的好处就是可以节省大量的内存空间。
+Fibnacci 509
 
+斐波那契数，通常用 F(n) 表示，形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和
+F(0) = 0，F(1) = 1
+F(n) = F(n - 1) + F(n - 2)，其中 n > 1
 
 :Author: Shangmengqi@tsingj.com
 :Last Modified by: Shangmengqi@tsingj.com
 """
 
-import time
+class Solution:
+    def fib(self, n: int) -> int:
+        a = 0
+        b = 1
+        i = 0
 
-class Fibnacci(object):
-    def __init__(self, all_num):
-        self.all_num = all_num
-        self.current_num = 0
-        self.a = 0
-        self.b = 1
+        while i <= n:
+            ret = a % (1e9 + 7)
+            a, b = b % (1e9 + 7), (a + b) % (1e9 + 7)
+            i += 1
 
-    def __iter__(self):
-        """
-        iter方法返回一个特殊的迭代器对象
-        :return:
-        """
-        return self
-
-    def __next__(self):
-        """
-        StopIteration 异常用于标识迭代的完成，防止出现无限循环的情况，在 __next__() 方法中我们可以设置在完成指定循环次数后触发 StopIteration 异常来结束迭代。
-        """
-        if self.current_num < self.all_num:
-            ret = self.a
-            self.a, self.b = self.b, self.a + self.b
-            self.current_num += 1
-            return ret
-        else:
-            raise StopIteration
-
-
-def main():
-    fibo = Fibnacci(45)
-
-    # 使用迭代器的方式一：for循环遍历
-    # for num in fibo:
-    #     print(num)
-    #     time.sleep(1)
-
-    # 使用迭代器的方式二：可以用list,tuple接受迭代器，转换成一个可迭代对象
-    fibo_list = list(fibo)
-    print(fibo_list)
-
-
-if __name__ == "__main__":
-    main()
+        return int(ret)
